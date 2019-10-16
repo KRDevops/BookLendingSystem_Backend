5494ab82299508b4dbb7f2005d744fcd3a48f71e
tsbalaji <tsbalaji10@gmail.com>
2019-10-16 19:46:40 +0530

    Fix

--- src/main/java/com/ing/bms/service/SchedulerServiceImpl.java
+++ src/main/java/com/ing/bms/service/SchedulerServiceImpl.java
@@ -1,10 +1,21 @@
 package com.ing.bms.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.ing.bms.repository.UserRepository;
 
 @Service
 public class SchedulerServiceImpl implements SchedulerService{
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(SchedulerServiceImpl.class);
+	
+	/*
+	 * @Autowired BookRepository bookRepository;
+	 */
+	
 	
 
 }
