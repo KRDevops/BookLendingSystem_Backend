2bf757fe6e8586a93c477aa9307c343ba61a2801
AVK Kiran Kumar <avk.kirankumar@gmail.com>
2019-10-17 16:08:05 +0530

    Adding Cross Origin

--- src/main/java/com/ing/bms/controller/BorrowedController.java
+++ src/main/java/com/ing/bms/controller/BorrowedController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import com.ing.bms.entity.Transaction;
 import com.ing.bms.service.BorrowedService;
 
 @RestController
+@CrossOrigin(allowedHeaders = { "*", "*/" }, origins = { "*", "*/" })
 @RequestMapping(value = "/api/v1")
 public class BorrowedController {
 	@Autowired
--- src/main/java/com/ing/bms/controller/SearchController.java
+++ src/main/java/com/ing/bms/controller/SearchController.java
@@ -3,6 +3,7 @@ package com.ing.bms.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,7 +13,8 @@ import com.ing.bms.dto.SearchDto;
 import com.ing.bms.service.SearchService;
 
 @RestController
-@RequestMapping(value = "/api")
+@CrossOrigin(allowedHeaders = { "*", "*/" }, origins = { "*", "*/" })
+@RequestMapping(value = "/api/v1")
 public class SearchController {
 	@Autowired
 	SearchService searchService;
